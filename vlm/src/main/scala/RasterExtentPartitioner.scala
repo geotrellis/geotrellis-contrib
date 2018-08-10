544455af5068a3b46195f8f03d08d7f8d2f99d6c
Jacob Bouffard <jbouffard@azavea.com>
2018-08-10 08:44:07 -0400

    Created the RasterExtentPartitioner object
    Signed-off-by: Jacob Bouffard <jbouffard@azavea.com>

---
+++ vlm/src/main/scala/RasterExtentPartitioner.scala
@@ -0,0 +1,46 @@
+package geotrellis.contrib.vlm
+
+
+import geotrellis.raster.RasterExtent
+
+import scala.collection.mutable.ArrayBuilder
+
+
+private[vlm] object RasterExtentPartitioner {
+  def partitionRasterExtents(
+    rasterExtents: Traversable[RasterExtent],
+    maxPartitionSize: Long
+  ): Array[Array[RasterExtent]] = {
+    val partition = ArrayBuilder.make[RasterExtent]
+    partition.sizeHintBounded(128, rasterExtents)
+    var partitionSize: Long = 0l
+    var partitionCount: Long = 0l
+    val partitions = ArrayBuilder.make[Array[RasterExtent]]
+
+    def finalizePartition() {
+      val res = partition.result
+      if (res.nonEmpty) partitions += res
+      partition.clear()
+      partitionSize = 0l
+      partitionCount = 0l
+    }
+
+    def addToPartition(rasterExtent: RasterExtent) {
+      partition += rasterExtent
+      partitionSize += rasterExtent.size
+      partitionCount += 1
+    }
+
+    for (rasterExtent <- rasterExtents) {
+      if ((partitionCount == 0) || (partitionSize + rasterExtent.size) < maxPartitionSize)
+        addToPartition(rasterExtent)
+      else {
+        finalizePartition()
+        addToPartition(rasterExtent)
+      }
+    }
+
+    finalizePartition()
+    partitions.result
+  }
+}
