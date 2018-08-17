bd608efce5569e14de787e0e5f9f3a92fac6b244
Jacob Bouffard <jbouffard@azavea.com>
2018-08-17 11:47:01 -0400

    Fixed the logic in WarpRasterSource

--- vlm/src/main/scala/WarpRasterSource.scala
+++ vlm/src/main/scala/WarpRasterSource.scala
@@ -13,35 +13,28 @@ import java.net.URI
 case class WarpRasterSource(
   base: RasterSource,
   crs: CRS,
-  targetLayout: LayoutDefinition,
   resampleMethod: ResampleMethod = NearestNeighbor
 ) extends RasterSource {
 
   def uri: URI = base.uri
-  def cols: Int = rasterExtent.cols
-  def rows: Int = rasterExtent.rows
   def cellType: CellType = base.cellType
   def bandCount: Int = base.bandCount
 
-  def extent: Extent = {
-    val oldProjectedExtent = ProjectedExtent(base.extent, base.crs)
-
-    oldProjectedExtent.reprojectAsPolygon(crs).envelope
-  }
-
-  //private val options = Reproject.Options(method = resampleMethod, parentGridExtent = Some(base.gridExtent))
-  private val options = Reproject.Options(method = resampleMethod, parentGridExtent = Some(targetLayout))
+  private val options = Reproject.Options(method = resampleMethod)
 
   private val transform = Transform(base.crs, crs)
   private val backTransform = Transform(crs, base.crs)
 
+  override def rasterExtent = ReprojectRasterExtent(base.rasterExtent, transform)
+  def extent = rasterExtent.extent
+
+  def cols: Int = rasterExtent.cols
+  def rows: Int = rasterExtent.rows
+
   def read(windows: Traversable[RasterExtent]): Iterator[Raster[MultibandTile]] = {
     val rasterExtents: Traversable[RasterExtent] =
       windows.map { case targetRasterExtent =>
-        val projectedExtent = ProjectedExtent(targetRasterExtent.extent, crs)
-
-        //val sourceExtent: Extent = projectedExtent.reprojectAsPolygon(base.crs).envelope
-        val sourceExtent: Extent = ReprojectRasterExtent.reprojectExtent(targetRasterExtent, backTransform)
+        val sourceExtent = ReprojectRasterExtent.reprojectExtent(targetRasterExtent, backTransform)
         val sourceGridBounds = base.rasterExtent.gridBoundsFor(sourceExtent, clamp = true)
 
         RasterExtent(sourceExtent, sourceGridBounds.width, sourceGridBounds.height)
