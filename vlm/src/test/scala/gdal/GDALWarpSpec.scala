3a04281845a84eb3fd75d52c6ed171da833988f0
Jacob Bouffard <jbouffard@azavea.com>
2018-09-18 09:01:26 -0400

    Expanded and cleaned up the GDALWarpSpec

--- vlm/src/test/scala/gdal/GDALWarpSpec.scala
+++ vlm/src/test/scala/gdal/GDALWarpSpec.scala
@@ -22,43 +22,17 @@ import org.gdal.osr.SpatialReference
 
 class GDALWarpSpec extends FunSpec with TestEnvironment with RasterMatchers {
   describe("Reprojecting a GDALRasterSource") {
-    //val uri = s"${new File("").getAbsolutePath()}/src/test/resources/img/aspect-tiled.tif"
-    val uri = "/tmp/test-file.tif"
+    val uri = s"${new File("").getAbsolutePath()}/src/test/resources/img/aspect-tiled.tif"
 
     val rasterSource = GDALRasterSource(uri)
-    val sourceDataset = GDAL.open(uri)
 
     val sourceTiff = GeoTiffReader.readMultiband(uri)
+    val sourceDataset = GDAL.open(uri)
 
     val reprojectedRasterExtent = {
       val re = ReprojectRasterExtent(rasterSource.rasterExtent, Transform(rasterSource.crs, LatLng))
       // stretch target raster extent slightly to avoid default case in ReprojectRasterExtent
       RasterExtent(re.extent, CellSize(re.cellheight * 1.1, re.cellwidth * 1.1))
-    }
-
-    val params =
-      new java.util.Vector(
-        java.util.Arrays.asList(
-          "-s_srs",
-          rasterSource.crs.toProj4String,
-          "-t_srs",
-          LatLng.toProj4String
-        )
-      )
-
-    val noDataValue: Option[Double] = {
-      val arr = Array.ofDim[java.lang.Double](1)
-      sourceDataset.GetRasterBand(1).GetNoDataValue(arr)
-
-      arr.head match {
-        case null => None
-        case value => Some(value.doubleValue)
-      }
-    }
-
-    val ct = noDataValue match {
-      case Some(nd) => FloatUserDefinedNoDataCellType(nd.toFloat)
-      case _ => FloatConstantNoDataCellType
     }
 
     def testReprojection(method: ResampleMethod) = {
@@ -70,49 +44,15 @@ class GDALWarpSpec extends FunSpec with TestEnvironment with RasterMatchers {
       val vrt =
         gdal.AutoCreateWarpedVRT(
           sourceDataset,
-          new SpatialReference(sourceDataset.GetProjection).ExportToWkt,
+          sourceDataset.GetProjection,
           sr.ExportToWkt,
           GDAL.deriveGDALResampleMethod(method)
         )
 
       val testBounds = GridBounds(0, 0, reprojectedRasterExtent.cols, reprojectedRasterExtent.rows).split(64,64).toSeq
 
-      val updatedParams = {
-        val update = params
-        update.add("-r")
-        update.add(s"${GDAL.deriveResampleMethodString(method)}")
-
-        val result = update
-
-        update.removeElement("-r")
-        update.removeElement(s"${GDAL.deriveResampleMethodString(method)}")
-
-        result
-      }
-
-      val options = new WarpOptions(updatedParams)
-      val dataset = gdal.Warp("/vsimem/gdal-warp-test", Array(sourceDataset), options)
-
-      //val geoTransform: Array[Double] = dataset.GetGeoTransform
       val geoTransform: Array[Double] = vrt.GetGeoTransform
-
-      /*
-      val cols = dataset.GetRasterXSize.toInt
-      val rows = dataset.GetRasterYSize.toInt
-
-      val extent = {
-        val xmin: Double = geoTransform(0)
-        val ymin: Double = geoTransform(3) + geoTransform(5) * rows
-        val xmax: Double = geoTransform(0) + geoTransform(1) * cols
-        val ymax: Double = geoTransform(3)
-
-        Extent(xmin, ymin, xmax, ymax)
-      }
-
-      val rasterExtent = RasterExtent(extent, cols, rows)
-
-      val testBounds = GridBounds(0, 0, cols, rows).split(64,64).toSeq
-      */
+      vrt.delete
 
       for (bound <- testBounds) yield {
         withClue(s"Read window ${bound}: ") {
@@ -137,56 +77,9 @@ class GDALWarpSpec extends FunSpec with TestEnvironment with RasterMatchers {
             )
           }
 
-          /*
-          val expected: Raster[MultibandTile] = {
-            val arr = Array.ofDim[Float](bound.width * bound.height)
-
-            dataset
-              .ReadRaster(
-                bound.colMin,
-                bound.rowMin,
-                bound.width,
-                bound.height,
-                bound.width,
-                bound.height,
-                gdalconstConstants.GDT_Float32,
-                arr,
-                Array(1)
-              )
-
-            val tile = MultibandTile(FloatArrayTile(arr, bound.width, bound.height, ct))
-
-            Raster(tile, targetExtent)
-          }
-          */
-
-          dataset.delete
-
-          //val actual = warpRasterSource.read(List(testRasterExtent)).next
-          val actual = {
-            val gb = testRasterExtent.gridBounds
-            val arr = Array.ofDim[Float](testRasterExtent.cols * testRasterExtent.rows)
-
-            vrt.ReadRaster(
-              gb.colMin,
-              gb.rowMin,
-              gb.width,
-              gb.height,
-              gb.width,
-              gb.height,
-              gdalconstConstants.GDT_Float32,
-              arr,
-              Array(1)
-            )
-
-            val tile = FloatArrayTile(arr, gb.width, gb.height, -9999.0.toFloat)
-
-            Raster(MultibandTile(tile), testRasterExtent.extent)
-          }
-
+          val actual = warpRasterSource.read(List(testRasterExtent)).next
 
           val expectedTile = expected.tile.band(0)
-          //val actualTile = actual.tile.band(0).reproject(actual.extent, sourceTiff.crs, LatLng).tile
           val actualTile = actual.tile.band(0)
 
           actual.extent.covers(expected.extent) should be (true)
@@ -194,11 +87,6 @@ class GDALWarpSpec extends FunSpec with TestEnvironment with RasterMatchers {
           actual.rasterExtent.extent.ymax should be (expected.rasterExtent.extent.ymax +- 0.00001)
           actual.rasterExtent.cellwidth should be (expected.rasterExtent.cellwidth +- 0.00001)
           actual.rasterExtent.cellheight should be (expected.rasterExtent.cellheight +- 0.00001)
-
-          println(s"\nThis is the expectedTile:")
-          println(NumericEncoder.encodeIntegrals(expectedTile))
-          println("\nThis is the actualTile:")
-          println(s"${NumericEncoder.encodeIntegrals(actualTile)}\n")
 
           assertEqual(actual, expected)
         }
@@ -211,6 +99,18 @@ class GDALWarpSpec extends FunSpec with TestEnvironment with RasterMatchers {
 
     it("should reproject using Bilinear") {
       testReprojection(Bilinear)
+    }
+
+    it("should produce the same thing as GDALRasterSource") {
+      val reprojectedImage = s"${new File("").getAbsolutePath()}/src/test/resources/img/aspect-tiled-3857.tif"
+
+      val gdalSource = GDALRasterSource(reprojectedImage)
+      val warpSource = rasterSource.withCRS(WebMercator)
+
+      val expectedTile = gdalSource.read().next.tile.band(0)
+      val actualTile = warpSource.read().next.tile.band(0)
+
+      assertEqual(expectedTile, actualTile)
     }
   }
 }
