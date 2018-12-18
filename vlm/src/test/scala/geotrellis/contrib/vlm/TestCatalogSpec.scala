22f462c18b6107bb18c81fa60a58a9204901c3ed
Peter Caisse <pcaisse@gmail.com>
2018-12-18 16:55:25 -0500

    Add reader tests for test catalog

--- vlm/src/test/scala/geotrellis/contrib/vlm/TestCatalogSpec.scala
+++ vlm/src/test/scala/geotrellis/contrib/vlm/TestCatalogSpec.scala
@@ -19,13 +19,41 @@ package geotrellis.contrib.vlm
 import TestCatalog._
 
 import org.scalatest.FunSpec
+import geotrellis.spark.io.{ValueReader, CollectionLayerReader}
+import geotrellis.raster.{Tile, MultibandTile}
+import geotrellis.spark._
+import geotrellis.spark.io._
+
+
 import java.io.File
 
+
 class TestCatalogSpec extends FunSpec with CatalogTestEnvironment {
+  val absOutputPath = s"file://${TestCatalog.outputPath}"
 
   describe("catalog test environment") {
     it("should create catalog before test is run") {
       assert(new File(TestCatalog.outputPath).exists)
+    }
+  }
+  describe("value reader") {
+    it("should be able to read test catalog") {
+      ValueReader(absOutputPath).reader[SpatialKey, Tile](LayerId("landsat", 0))
+    }
+    it("should be unable to read non-existent test catalog") {
+      assertThrows[AttributeNotFoundError] {
+        ValueReader(absOutputPath).reader[SpatialKey, Tile](LayerId("INVALID", 0))
+      }
+    }
+  }
+  describe("collection layer reader") {
+    it("should be able to read test catalog") {
+      CollectionLayerReader(absOutputPath).read[SpatialKey, MultibandTile, TileLayerMetadata[SpatialKey]](LayerId("landsat", 0))
+    }
+    it("should be unable to read non-existent test catalog") {
+      assertThrows[LayerNotFoundError] {
+        CollectionLayerReader(absOutputPath).read[SpatialKey, MultibandTile, TileLayerMetadata[SpatialKey]](LayerId("INVALID", 0))
+      }
     }
   }
 
