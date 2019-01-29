c0d2579162538d2d08c3106ab29c4144a459fa0e
Jacob Bouffard <jbouffard@azavea.com>
2019-01-29 11:41:52 -0500

    Created the ReadingSource companion object

--- vlm/src/main/scala/geotrellis/contrib/vlm/ReadingSource.scala
+++ vlm/src/main/scala/geotrellis/contrib/vlm/ReadingSource.scala
@@ -5,3 +5,11 @@ case class ReadingSource(
   source: RasterSource,
   sourceToTargetBand: Map[Int, Int]
 ) extends Serializable
+
+object ReadingSource {
+  def apply(source: RasterSource, sourceBand: Int, targetBand: Int): ReadingSource =
+    ReadingSource(source, Map(sourceBand -> targetBand))
+
+  def apply(source: RasterSource, targetBand: Int): ReadingSource =
+    apply(source, 0, targetBand)
+}
