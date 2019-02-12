e2df01b91027b993bcf150893e27e21250ad8410
Jacob Bouffard <jbouffard@azavea.com>
2019-02-12 09:58:35 -0500

    Added the readMethod parameter to RasterViewOptions

--- vlm/src/main/scala/geotrellis/contrib/vlm/RasterViewOptions.scala
+++ vlm/src/main/scala/geotrellis/contrib/vlm/RasterViewOptions.scala
@@ -1,11 +1,13 @@
 package geotrellis.contrib.vlm
 
 import geotrellis.proj4.CRS
-import geotrellis.raster.{RasterExtent, CellType}
+import geotrellis.vector.Extent
+import geotrellis.raster.{RasterExtent, CellType, Raster, MultibandTile}
 
 
 private[vlm] case class RasterViewOptions(
   crs: Option[CRS] = None,
   cellType: Option[CellType] = None,
-  rasterExtent: Option[RasterExtent] = None
+  rasterExtent: Option[RasterExtent] = None,
+  readMethod:  Option[(Extent, Seq[Int]) => Option[Raster[MultibandTile]]] = None
 )
