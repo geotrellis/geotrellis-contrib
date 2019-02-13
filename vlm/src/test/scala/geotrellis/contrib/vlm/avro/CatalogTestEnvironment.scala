5c50658db971d8a33182035df9c34247afbe1906
Grigory Pomadchin <gr.pomadchin@gmail.com>
2019-02-13 11:51:32 +0300

    Fix GeoTrellis catalog generation as it may cause tests failure

--- vlm/src/test/scala/geotrellis/contrib/vlm/avro/CatalogTestEnvironment.scala
+++ vlm/src/test/scala/geotrellis/contrib/vlm/avro/CatalogTestEnvironment.scala
@@ -16,7 +16,6 @@
 
 package geotrellis.contrib.vlm.avro
 
-import TestCatalog._
 import geotrellis.spark.testkit.TestEnvironment
 import org.scalatest.Suite
 import java.io.File
@@ -24,18 +23,13 @@ import java.io.File
 trait CatalogTestEnvironment extends TestEnvironment { self: Suite =>
 
   override def beforeAll() = {
-    val multibandFile = new File(TestCatalog.multibandOutputPath)
-    if (!(multibandFile).exists) {
-      TestCatalog.createMultiband
-    } else {
-      println(s"Test multi-band catalog exists at: $multibandFile")
-    }
-    val singlebandFile = new File(TestCatalog.singlebandOutputPath)
-    if (!(singlebandFile).exists) {
-      TestCatalog.createSingleband
-    } else {
-      println(s"Test single-band catalog exists at: $singlebandFile")
-    }
+    val multibandDir = new File(TestCatalog.multibandOutputPath)
+    // length >= 2 means that in the directory there are at least two folders - attributes and at least one layer folder
+    if (!(multibandDir.exists && multibandDir.list().length >= 2)) TestCatalog.createMultiband
+    else println(s"Test multi-band catalog exists at: $multibandDir")
+    val singlebandDir = new File(TestCatalog.singlebandOutputPath)
+    if (!(singlebandDir.exists && singlebandDir.list().length >= 2)) TestCatalog.createSingleband
+    else println(s"Test single-band catalog exists at: $singlebandDir")
   }
 
 }
