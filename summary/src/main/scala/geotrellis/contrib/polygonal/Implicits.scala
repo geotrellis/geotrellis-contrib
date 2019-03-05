1344cb5fd9275bd70525e4c6faab03a6a6d5db8b
Eugene Cheipesh <echeipesh@gmail.com>
2019-03-05 17:34:10 -0500

    Expose Rasterizer.Options as parameter to polygonal summary

--- summary/src/main/scala/geotrellis/contrib/polygonal/Implicits.scala
+++ summary/src/main/scala/geotrellis/contrib/polygonal/Implicits.scala
@@ -7,16 +7,15 @@ import cats._
 
 object Implicits {
   implicit val poloygonalSummaryForRasterTile: PolygonalSummary[Raster[Tile]] = new PolygonalSummary[Raster[Tile]] {
-    // TODO: docstring
     def polygonalSummary[G <: Geometry, R: CellAccumulator: Monoid](
       self: Raster[Tile],
-      feature: Feature[G, R]
+      feature: Feature[G, R],
+      options: Rasterizer.Options
     ): Feature[G, R] = {
       import CellAccumulator.ops._
       var result: R = feature.data
       val geom = feature.geom
 
-      val options = Rasterizer.Options(includePartial =  true, sampleType = PixelIsArea)
       Rasterizer.foreachCellByGeometry(geom, self.rasterExtent, options)  { (col: Int, row: Int) =>
         val v = self.tile.getDouble(col, row)
         if (isData(v)) result = result.add(v)
