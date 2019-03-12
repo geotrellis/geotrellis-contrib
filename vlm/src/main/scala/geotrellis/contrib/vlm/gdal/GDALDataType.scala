6d7876f3f03b5609579a5ad5bb27f67e2d600886
James McClain <jmcclain@azavea.com>
2019-03-12 16:45:48 -0400

    Use More Advanced CellType Calculation

--- vlm/src/main/scala/geotrellis/contrib/vlm/gdal/GDALDataType.scala
+++ vlm/src/main/scala/geotrellis/contrib/vlm/gdal/GDALDataType.scala
@@ -22,18 +22,18 @@ import com.azavea.gdal.GDALWarp
 object GDALDataType {
   val types =
     List(
-      GDALWarp.GDT_Unknown,
-      GDALWarp.GDT_Byte,
-      GDALWarp.GDT_UInt16,
-      GDALWarp.GDT_Int16,
-      GDALWarp.GDT_UInt32,
-      GDALWarp.GDT_Int32,
-      GDALWarp.GDT_Float32,
-      GDALWarp.GDT_Float64,
-      GDALWarp.GDT_CInt16,
-      GDALWarp.GDT_CInt32,
-      GDALWarp.GDT_CFloat32,
-      GDALWarp.GDT_CFloat64
+      UnknownType,
+      TypeByte,
+      TypeUInt16,
+      TypeInt16,
+      TypeUInt32,
+      TypeInt32,
+      TypeFloat32,
+      TypeFloat64,
+      TypeCInt16,
+      TypeCInt32,
+      TypeCFloat32,
+      TypeCFloat64
     )
 
   implicit def intToGDALDataType(i: Int): GDALDataType =
@@ -52,15 +52,15 @@ abstract sealed class GDALDataType(val code: Int) {
 }
 
 // https://github.com/geotrellis/gdal-warp-bindings/blob/9d75e7c65c4c8a0c2c39175a75656bba458a46f0/src/main/java/com/azavea/gdal/GDALWarp.java#L26-L38
-case object UnknownType extends GDALDataType(0)
-case object TypeByte extends GDALDataType(1)
-case object TypeUInt16 extends GDALDataType(2)
-case object TypeInt16 extends GDALDataType(3)
-case object TypeUInt32 extends GDALDataType(4)
-case object TypeInt32 extends GDALDataType(5)
-case object TypeFloat32 extends GDALDataType(6)
-case object TypeFloat64 extends GDALDataType(7)
-case object TypeCInt16 extends GDALDataType(8)
-case object TypeCInt32 extends GDALDataType(9)
-case object TypeCFloat32 extends GDALDataType(10)
-case object TypeCFloat64 extends GDALDataType(11)
+case object UnknownType extends GDALDataType(GDALWarp.GDT_Unknown)
+case object TypeByte extends GDALDataType(GDALWarp.GDT_Byte)
+case object TypeUInt16 extends GDALDataType(GDALWarp.GDT_UInt16)
+case object TypeInt16 extends GDALDataType(GDALWarp.GDT_Int16)
+case object TypeUInt32 extends GDALDataType(GDALWarp.GDT_UInt32)
+case object TypeInt32 extends GDALDataType(GDALWarp.GDT_Int32)
+case object TypeFloat32 extends GDALDataType(GDALWarp.GDT_Float32)
+case object TypeFloat64 extends GDALDataType(GDALWarp.GDT_Float64)
+case object TypeCInt16 extends GDALDataType(GDALWarp.GDT_CInt16)
+case object TypeCInt32 extends GDALDataType(GDALWarp.GDT_CInt32)
+case object TypeCFloat32 extends GDALDataType(GDALWarp.GDT_CFloat32)
+case object TypeCFloat64 extends GDALDataType(GDALWarp.GDT_CFloat64)
