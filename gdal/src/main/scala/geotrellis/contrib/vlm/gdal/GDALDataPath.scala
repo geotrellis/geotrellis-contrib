a063ae57312a5348c3a37aebaddd42820d19085e
Jacob Bouffard <jbouffard@azavea.com>
2019-06-26 13:44:27 -0400

    Updated GDALDataPath so that it uses GDALPathTypes
    Signed-off-by: Jacob Bouffard <jbouffard@azavea.com>

--- gdal/src/main/scala/geotrellis/contrib/vlm/gdal/GDALDataPath.scala
+++ gdal/src/main/scala/geotrellis/contrib/vlm/gdal/GDALDataPath.scala
@@ -18,6 +18,8 @@ package geotrellis.contrib.vlm.gdal
 
 import geotrellis.contrib.vlm.DataPath
 
+import io.lemonlabs.uri.{UrlWithAuthority, Path}
+
 import java.io.File
 import java.net.MalformedURLException
 
@@ -47,6 +49,12 @@ case class GDALDataPath(
   import Schemes._
   import Patterns._
 
+  val gdalPath =
+    UrlWithAuthority.parseOption(path) match {
+      case Some(uri) => URIPath(uri)
+      case None => LocalPath(Path.parse(path))
+    }
+
   private val badPrefixes: List[String] =
     List("gt+", "gtiff+")
 
@@ -71,34 +79,31 @@ case class GDALDataPath(
     s"Cannot create a GDALDataPath that points to a GeoTrellis catalog: $path"
   )
 
-  private val servicePrefixes: List[String] =
-    List(
-      "gdal+",
-      "zip+",
-      "gzip+",
-      "gz+",
-      "tar+"
-    )
+  val firstScheme: Option[String] =
+    gdalPath.firstScheme match {
+      case Some(_) => gdalPath.firstScheme
+      case None =>
+        val Array(_, extension) =
+          gdalPath
+            .targetFile
+            .split('.')
+            .map { _.toLowerCase }
 
-  private val strippedPath: String =
-    servicePrefixes
-      .filter { path.startsWith }
-      .headOption match {
-        case Some(prefix) => path.splitAt(prefix.size)._2
-        case None => path
-      }
+        val extensionScheme: Option[String] =
+          if (gdalPath.targetsCompressedFile)
+            COMPRESSED_FILE_TYPES
+              .filter { fileType => fileType == extension }
+              .headOption
+          else
+            None
 
-  val scheme: Option[String] =
-    SCHEME_PATTERN.findFirstIn(strippedPath)
-
-  private val schemeString: String =
-    scheme match {
-      case Some(targetScheme) => targetScheme.toLowerCase
-      case None => ""
+        extensionScheme.flatMap { FILE_TYPE_TO_SCHEME.get }
     }
 
+  val secondScheme: String = gdalPath.secondScheme
+
   private val isLocal: Boolean =
-    schemeString.contains(FILE) || schemeString == ""
+    secondScheme.contains(FILE) || secondScheme.isEmpty
 
   // Trying to read something locally on Windows matters
   // because of how file paths on Windows are formatted.
@@ -106,182 +111,35 @@ case class GDALDataPath(
   private val onLocalWindows: Boolean =
     System.getProperty("os.name").toLowerCase == "win" && isLocal
 
-  val targetPath: Option[String] =
-    scheme match {
-      case Some(_) =>
-        if (onLocalWindows)
-          WINDOWS_LOCAL_PATH_PATTERN.findFirstIn(strippedPath)
-        else
-          DEFAULT_PATH_PATTERN.findFirstIn(strippedPath)
-      case None =>
-        Some(strippedPath)
-    }
-
-  private val targetPathString: String =
-    targetPath match {
-      case Some(pathString) => pathString
-      case None =>
-        throw new MalformedURLException(
-          s"Invalid URI passed into the GDALRasterSource constructor: ${strippedPath}." +
-          s"Check geotrellis.contrib.vlm.gdal.VSIPath constrains, " +
-          s"or pass VSI formatted String into the GDALRasterSource constructor manually."
-        )
-    }
-
-  val targetFileName: String = {
-    val filePath =
-      if (onLocalWindows)
-        // File isn't able to find the name of the
-        // target file if '\' are in the path, so we
-        // need to replace them with '/'.
-        targetPathString.replaceAll("""\\""", "/")
-      else
-        targetPathString
-
-    new File(filePath).getName
-  }
-
-  private val targetsCompressedDirectory: Boolean =
-    COMPRESSED_FILE_TYPES
-      .map { extension => targetFileName.contains(extension) }
-      .reduce { _ || _ }
-
-  private val targetsCompressedFile: Boolean =
-    targetFileName.contains(compressedFileDelimiter)
-
-  val authority: Option[String] =
-    AUTHORITY_PATTERN.findFirstIn(strippedPath)
-
-  private val authorityString: String =
-    authority match {
-      case Some(authority) => authority
-      case None => ""
-    }
-
-  val userInfo: Option[String] =
-    USER_INFO_PATTERN.findFirstIn(strippedPath)
-
-  private val userInfoString: String =
-    userInfo match {
-      case Some(info) => info
-      case None => ""
-    }
-
   private val formattedPathString: String =
-    if (!targetsCompressedFile)
-      targetPathString
-    else {
+    if (gdalPath.targetsNestedFile) {
       val formattedFileName =
         if (onLocalWindows)
-          targetFileName.replace(compressedFileDelimiter, """\\""")
+          gdalPath.targetFile.replace(compressedFileDelimiter, """\\""")
         else
-          targetFileName.replace(compressedFileDelimiter, "/")
+          gdalPath.targetFile.replace(compressedFileDelimiter, "/")
 
       val pathWithoutFileName =
-        targetPathString.substring(0, targetPathString.size - formattedFileName.size)
+        gdalPath.path.substring(0, gdalPath.path.size - formattedFileName.size)
 
       pathWithoutFileName + formattedFileName
-    }
-
-  // The given path can contain 0, 1, or 2 different schemes. Any one of these
-  // can be chained together with other handlers. Thus, we must find out what
-  // our first and second scheme is in order to properly format the path.
-  //
-  // The firstScheme is only used when there is a chained operation, and it
-  // always points to the handler needed to read the target file
-  // (ie. vsizip, vsitar, etc).
-  //
-  // The secondScheme points the to the handler needed to access the target
-  // backend (ie. vsis3, vsihdfs, etc). The only time there isn't a
-  // secondScheme is when the user is reading from the local file system.
-  val (firstScheme, secondScheme): (Option[String], Option[String]) =
-
-    // Schemes that contain "+" point to a file that's either compressed or
-    // within a compressed file itself.
-    if (schemeString.contains("+")) {
-      val firstScheme = FIRST_SCHEME_PATTERN.findFirstIn(schemeString)
-      val secondScheme = SECOND_SCHEME_PATTERN.findFirstIn(strippedPath)
-
-      (firstScheme, secondScheme)
-
-    // The scheme does not contain "+", but it still points some kind
-    // of compressed data. In the case where we're reading data from
-    // a local file system, we can drop the scheme for that backend
-    // as it's not neeeded to read the file.
-    } else if (targetsCompressedDirectory) {
-
-      // The data just points to a compressed file and not data within
-      // it.
-      if (!targetsCompressedFile) {
-        val extensionIndex = targetFileName.lastIndexOf(".") + 1
-        val extension = targetFileName.substring(extensionIndex)
-
-        (
-          FILE_TYPE_TO_SCHEME.get(extension),
-          if (isLocal) None else Some(schemeString)
-        )
-
-      // The path points to data within a compressed file that needs
-      // to be read.
-      } else {
-        val compressionName = targetFileName.split(compressedFileDelimiter).head
-
-        val extensionIndex = compressionName.lastIndexOf(".") + 1
-        val extension = compressionName.substring(extensionIndex)
-
-        (
-          FILE_TYPE_TO_SCHEME.get(extension),
-          if (isLocal) None else Some(schemeString)
-        )
-      }
-
-    // We're not reading any compressed data
     } else
-      (None, if (isLocal) None else Some(schemeString))
+      gdalPath.path
 
   private val firstVSIScheme: String =
-    firstScheme.flatMap { FILE_TYPE_TO_SCHEME.get } match {
+    firstScheme match {
       case Some(firstHalf) => s"/vsi$firstHalf/"
       case None => ""
     }
 
   private val secondVSIScheme: String = {
-    val secondSchemeString: String =
-      secondScheme match {
-        case Some(scheme) => scheme
-        case None => ""
-      }
-
-    secondSchemeString match {
-      case (FTP | HTTP | HTTPS) =>
-        if (targetsCompressedFile)
-          s"/vsicurl/$secondSchemeString://$formattedPathString"
-        else
-          s"/vsicurl/$strippedPath"
-      case S3 =>
-        s"/vsis3/$formattedPathString"
-      case GS =>
-        s"/vsigs/$formattedPathString"
-      case (WASB | WASBS) =>
-        val azurePath = {
-          val authoritySize = authorityString.size
-          val substring = formattedPathString.substring(authorityString.size)
-
-          if (substring.startsWith("/")) substring.drop(1) else substring
-        }
-
-        s"/vsiaz/$userInfoString/$azurePath"
-      case HDFS =>
-        if (targetsCompressedFile)
-          s"/vsihdfs/hdfs://$formattedPathString"
-        else
-          s"/vsihdfs/$strippedPath"
-      case FILE =>
-        formattedPathString
-      case _ =>
-        // If no scheme is found, assume the path is
-        // a relative path to a file
-        formattedPathString
+    secondScheme match {
+      case (FTP | HTTP | HTTPS) => s"/vsicurl/$formattedPathString"
+      case S3 => s"/vsis3/$formattedPathString"
+      case GS => s"/vsigs/$formattedPathString"
+      case (WASB | WASBS) => s"/vsiaz/$formattedPathString"
+      case HDFS => s"/vsihdfs/$formattedPathString"
+      case _ => formattedPathString
     }
   }
 
