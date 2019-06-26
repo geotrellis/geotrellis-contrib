96deaa564d4ffb0a9c4d7eeca70af2af880cf963
Jacob Bouffard <jbouffard@azavea.com>
2019-06-26 13:42:37 -0400

    Created the GDALPathType trait and subclasses
    Signed-off-by: Jacob Bouffard <jbouffard@azavea.com>

---
+++ gdal/src/main/scala/geotrellis/contrib/vlm/gdal/GDALPathType.scala
@@ -0,0 +1,86 @@
+package geotrellis.contrib.vlm.gdal
+
+import io.lemonlabs.uri.{Path, UrlWithAuthority}
+
+
+sealed trait GDALPathType {
+  def path: String
+  def targetFile: String
+  def targetsCompressedFile: Boolean
+  def targetsNestedFile: Boolean
+  def scheme: String
+
+  def firstScheme: Option[String]
+  def secondScheme: String
+}
+
+case class LocalPath(localPath: Path) extends GDALPathType {
+  def path: String = localPath.toString
+
+  private val pathParts: Vector[String] = localPath.parts
+  def targetFile: String = pathParts(pathParts.size - 1)
+
+  def targetsCompressedFile: Boolean =
+    Schemes.COMPRESSED_FILE_TYPES
+      .map { targetFile.endsWith(_) }
+      .reduce { _ || _ }
+
+  def targetsNestedFile: Boolean = false
+
+  def scheme: String = "file"
+  def firstScheme: Option[String] = None
+  def secondScheme: String = scheme
+}
+
+case class URIPath(uri: UrlWithAuthority) extends GDALPathType {
+  import Schemes._
+
+  private val pathParts: Vector[String] =
+    uri.path.parts
+
+  def targetFile: String =
+    pathParts(pathParts.size - 1)
+
+  private val targetedFileCompressed: Boolean =
+    COMPRESSED_FILE_TYPES
+      .map { targetFile.endsWith(_) }
+      .reduce { _ || _ }
+
+  def scheme: String =
+    uri.schemeOption match {
+      case Some(str) => str
+      case None => ""
+    }
+
+  val (firstScheme, secondScheme): (Option[String], String) =
+    if (scheme.contains("+")) {
+      val Array(first, second) = scheme.split('+')
+      (Some(first), second)
+    } else
+      (None, scheme)
+
+  private val firstSchemeCompressed: Boolean =
+    firstScheme match {
+      case Some(sch) => "gdal" != firstScheme
+      case None => false
+    }
+
+  def targetsNestedFile: Boolean =
+    firstSchemeCompressed && !targetedFileCompressed
+
+  def targetsCompressedFile: Boolean =
+    targetsNestedFile || targetedFileCompressed
+
+  def path: String =
+    secondScheme match {
+      case (FTP | HTTP | HTTPS | HDFS) =>
+        s"$secondScheme://${uri.authority}${uri.path}"
+      case (WASB | WASBS) =>
+        uri.user match {
+          case Some(info) => s"${info}${uri.path}"
+          case None => s"${uri.authority}${uri.path}"
+        }
+      case _ =>
+        s"${uri.authority}${uri.path}"
+    }
+}
