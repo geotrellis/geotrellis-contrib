921aee51e9586cf90b9154198587eda45c068fca
Jacob Bouffard <jbouffard@azavea.com>
2019-06-26 15:06:14 -0400

    Cleaned up logic in GeoTrellisDataPath
    Signed-off-by: Jacob Bouffard <jbouffard@azavea.com>

--- vlm/src/main/scala/geotrellis/contrib/vlm/avro/GeoTrellisDataPath.scala
+++ vlm/src/main/scala/geotrellis/contrib/vlm/avro/GeoTrellisDataPath.scala
@@ -19,6 +19,8 @@ package geotrellis.contrib.vlm.avro
 import geotrellis.contrib.vlm.DataPath
 import geotrellis.spark.LayerId
 
+import io.lemonlabs.uri.UrlWithAuthority
+
 
 /** Represents a path that points to a GeoTrellis layer saved in a catalog.
  *
@@ -37,50 +39,29 @@ import geotrellis.spark.LayerId
  *  @note The order of the query parameters does not matter.
  */
 case class GeoTrellisDataPath(path: String) extends DataPath {
-  private val servicePrefix: String = "gt+"
-
   private val layerNameParam: String = "layer"
   private val zoomLevelParam: String = "zoom"
   private val bandCountParam: String = "band_count"
 
-  require(path.contains(s"$layerNameParam="), s"The layer query parameter must be in the given path: $path")
+  private val uri = UrlWithAuthority.parse(path)
+  private val queryString = uri.query
 
-  // TODO: Support having the zoom parameter be optional
-  require(path.contains(s"$zoomLevelParam="), s"The zoom query parameter must be in the given path: $path")
-
-  private val strippedPath: String =
-    if (path.startsWith(servicePrefix))
-      path.splitAt(servicePrefix.size)._2
-    else
-      path
-
-  val Array(catalogPath, queryParams) = strippedPath.split('?')
-
-  private val brokenUpParams: Array[(String, String)] =
-    queryParams
-      .split('&')
-      .map {
-        _.split('=') match {
-          case Array(k, v) => (k, v)
-        }
-      }
+  val catalogPath: String =
+    uri.schemeOption match {
+      case Some(scheme) => s"$scheme://${uri.authority}${uri.path}"
+      case None => throw new Exception(s"The given path requires a scheme: $path")
+    }
 
   /** The name of the target layer */
-  val layerName =
-    brokenUpParams
-      .filter { case (k, _) => k == layerNameParam }
-      .head
-      ._2
-
-  private val mappedQueryParams: Map[String, Int] =
-    (brokenUpParams.toMap - layerNameParam).mapValues { _.toInt }
+  val layerName = queryString.param(layerNameParam).get
 
   /** The zoom level of the target layer */
-  val zoomLevel: Option[Int] = mappedQueryParams.get(zoomLevelParam)
+  val zoomLevel: Option[Int] = queryString.param(zoomLevelParam).map { _.toInt }
 
   /** The band count of the target layer */
-  val bandCount: Option[Int] = mappedQueryParams.get(bandCountParam)
+  val bandCount: Option[Int] = queryString.param(bandCountParam).map { _.toInt }
 
+  /** The [[LayerId]] associated with the given layer */
   val layerId: LayerId = LayerId(layerName, zoomLevel.get)
 }
 
